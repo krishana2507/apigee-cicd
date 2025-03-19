56e2f0620aab28e49242b403bc6dac761d2d15ea
krishana2507 <130963813+krishana2507@users.noreply.github.com>
2025-03-19 15:18:46 +0530

    Update rollback.sh

--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-set -e
+echo "Rolling back to previous stable version..."
 
-ORG=$(jq -r '.org' config/apigee-config.json)
-ENV=$(jq -r '.env' config/apigee-config.json)
+# Fetch latest stable revision
+LAST_REVISION=$(apigeecli apis get --org my-apigee-org --name my-proxy --token "$(gcloud auth print-access-token)" | jq -r '.revision[-2]')
 
-# Rollback last deployment
-apigeecli deployments rollback -o "$ORG" -e "$ENV"
+# Deploy last stable revision
+apigeecli apis deploy --org my-apigee-org --env test --name my-proxy --revision $LAST_REVISION --token "$(gcloud auth print-access-token)"
+
+echo "Rollback to revision $LAST_REVISION completed!"
